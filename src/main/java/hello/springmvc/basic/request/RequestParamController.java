ac8f470d2a5d1eebacf86318fa24391ade8c8e79
JinYeongKim <handayoung11@gmail.com>
2023-03-20 09:44:48 +0900

    @ModelAttribute를 생략하여 요청 파라미터를 객체로 변환해서 받아오는 /model-attribute-v2 api 작성

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -84,4 +84,11 @@ public class RequestParamController {
         log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
         return "ok";
     }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        return "ok";
+    }
 }
