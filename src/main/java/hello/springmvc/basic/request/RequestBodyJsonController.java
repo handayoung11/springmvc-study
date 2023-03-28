c6ed950740c76fccb3b6ecd65fb122d8f435e235
JinYeongKim <handayoung11@gmail.com>
2023-03-28 15:42:09 +0900

    HttpServletRequest를 통해 messagebody를 추출하는 /request-body-json-v1 api 작성

---
+++ src/main/java/hello/springmvc/basic/request/RequestBodyJsonController.java
@@ -0,0 +1,32 @@
+package hello.springmvc.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.springmvc.basic.HelloData;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@Controller
+public class RequestBodyJsonController {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @PostMapping("/request-body-json-v1")
+    public void requestBodyJsonV1(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        ServletInputStream in = req.getInputStream();
+        String messageBody = StreamUtils.copyToString(in, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}", messageBody);
+        HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+        log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+        resp.getWriter().write("ok");
+    }
+}
